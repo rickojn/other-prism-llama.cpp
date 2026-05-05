4922af919984b973849a0289f2c91b1767efa32d
Patrick <rickojn@hotmail.com>
2026-05-05 21:48:14 +0100

    add kwarg

--- common/common.h
+++ common/common.h
@@ -571,6 +571,7 @@ struct common_params {
     int32_t n_ctx_checkpoints   = 32;    // max number of context checkpoints per slot
     int32_t checkpoint_every_nt = 8192;  // make a checkpoint every n tokens during prefill
     int32_t cache_ram_mib       = 8192;  // -1 = no limit, 0 - disable, 1 = 1 MiB, etc.
+    bool    qlazy               = false; // whether to ignore thinking tags when prefix matching
 
     std::string hostname      = "127.0.0.1";
     std::string public_path   = "";                                                                         // NOLINT
